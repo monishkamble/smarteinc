07ef54fdec08f109add5c3d52e183bca2c5db203
Monish Kamble <monish.kamble@yahoo.com>
2021-08-22 21:27:12 +0530

    1. Added comments explaining the solution

--- Question1/Solution.sql
+++ Question1/Solution.sql
@@ -1,3 +1,6 @@
+-- I have imported the data from india_phoneno_not_null file into a SQLite database with smarte.sqlite
+-- Please find below query to get the expected output
+
 select City, State, Country, ('0'||CityCode) as CityCode, Records from
 (select City, State, Country, substr(PhoneNo,0,3) as CityCode, count(*) as Records
 from Phone_Records
